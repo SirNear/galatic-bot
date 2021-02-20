ca30f83785ea46f9e4e25c57fe7501164f81b4d5
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-19 20:22:56 -0400

    Update clearwarn.js

--- commands/clearwarn.js
+++ commands/clearwarn.js
@@ -17,7 +17,12 @@ module.exports.run = async (client, message, args) => {
 
   let modRole = message.guild.roles.cache.get(rolestaff.replace(/[<@&>]/g, ""))
   
-   if(message.member.roles.cache.has(modRole)) return error.noStaffRole(message)
+   if(!message.member.roles.cache.has(modRole)) { 
+
+	error.noStaffRole(message)
+
+   }else {
+
    if(wUser === mb) return error.autoClearWarn(message)
    if(warn.warnNumber == '0') return error.noWarn(message)
    
@@ -36,7 +41,7 @@ module.exports.run = async (client, message, args) => {
   message.channel.send(`***Todos os avisos de ${wUser} Foram retirados !***`)
   canal.send(embed)
   
-    
+   }
 }
 
 exports.config = {
