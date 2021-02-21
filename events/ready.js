5c07e528b11876886cafac599ba755e8e97c9809
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-21 00:44:11 -0400

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,37 @@
+const Event = require('../Structures/Event');
+
+module.exports = class extends Event {
+
+	constructor(...args) {
+		super(...args, {
+			once: true
+		});
+	}
+
+	run() {
+		console.log([
+			`Logado em ${this.client.user.tag}`,
+			`${this.client.commands.size} comandos carregados!`,
+			`${this.client.events.size} eventos carregados!`
+		].join('\n'));
+	}
+  
+   let status = [
+    {name:`Meu poder a ${client.users.size} pessoas em todo o Galatic Coffe!`, type: 'STREAMING', url: 'https://twitch.tv/galatic'},
+    {name:'Pó de café na pia', type: 'PLAYING'},
+    {name:`Alegria para os meus filhos`, type: 'STREAMING', url: 'https://twitch.tv/galatic'},
+    {name:`Observando ${client.channels.size} mundos diferentes!`, type: 'PLAYING'}
+   
+  ];
+  
+  
+  function setStatus() {
+    let altstatus = status[Math.floor(Math.random()*status.length)]
+     client.user.setPresence({game: altstatus})
+    }
+
+  setStatus();
+  setInterval(() => setStatus(), 7000)
+})
+
+};
