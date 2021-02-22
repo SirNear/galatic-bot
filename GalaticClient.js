5af07dc804dd9bd614f4653ba9e8ebe4ad9007b0
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-22 18:16:30 -0400

    Update GalaticClient.js

--- GalaticClient.js
+++ GalaticClient.js
@@ -3,6 +3,7 @@ const { Client, Collection } = require("discord.js")
 const { readdir } = require("fs")
 const config = require('./config.json')
 const EventManager = require('./structures/EventManager.js')
+const client = new Discord.Client()
 
 module.exports = class GalaticClient extends Client {
 
