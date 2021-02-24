2d82011d3859d59bfe854e30938c3d3a51b7237a
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-24 14:54:15 -0400

    Update message.js

--- events/message.js
+++ events/message.js
@@ -18,7 +18,7 @@ module.exports = class MessageReceive {
 
 		
          if(!userData) {
-              uD = this.client.database.userData({
+              this.client.database.userData({
                  _id: message.author.id,
                   uid: message.author.id,
                   uName: message.author.username,
