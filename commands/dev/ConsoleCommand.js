83407e7d0c5379ee6f5ca8997c0674aa5750c13d
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-24 21:21:41 -0400

    Create ConsoleCommand.js

---
+++ commands/dev/ConsoleCommand.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require('discord.js');
+const Command = require('../../structures/Command');
+const error = require('../../api/error.js')
+const color = require('../../api/colors.json')
+const { inspect } = require('util')
+
+
+module.exports = class ConsoleCommand extends Command {
+
+	constructor(client) {
+		super(client, {
+			name: "console",
+			category: "dev",
+			aliases: ["logs",'cmd'],
+			UserPermission: null,
+			clientPermission: null,
+			OnlyDevs: true
+		})
+	}
+  
+ async run({ message, args, client, server}) {
+
+  console.stdlog = console.log.bind(console);
+  console.logs = [];
+  console.log = function(){
+      console.logs.push(Array.from(arguments));
+      console.stdlog.apply(console, arguments);
+  }
+   
+   message.channel.send(console.log)
+   
+ }
+}
+  
