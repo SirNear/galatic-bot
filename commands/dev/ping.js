09156f56fa0df9fd7dc1fd1fcac558acf66096cc
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-25 08:41:07 -0400

    Create ping.js

---
+++ commands/dev/ping.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const Command = require('../../structures/Command');
+const error = require('../../api/error.js')
+
+module.exports = class ping extends Command {
+	constructor(client) {
+		super(client, {
+			name: "ping",
+			category: "dev",
+			aliases: [''],
+			UserPermission: null,
+			clientPermission: null,
+			OnlyDevs: false
+		})
+	}
+  
+async run({ message, args, client, server}) {
+
+  message.channel.send('**Calculando..**').then(msg => {
+  
+  const embed = new Discord.MessageEmbed() 
+    .setAuthor('Ping? Pong!')
+    .setDescription(`Ping: \`\`${Math.round(client.ws.ping)}\`\`ms! \n Latência da API: \`\`${msg.createdTimestamp - message.createdTimestamp}\`\`ms! `)
+    .setColor('36393e');
+  
+      msg.edit(embed)
+  
+   })
+  }
+}
