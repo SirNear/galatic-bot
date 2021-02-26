7da90fa601afca7fd022b11c67ce16a62937c6ad
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-26 19:51:29 -0400

    Update warn.js

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const Command = require('../../structures/Command');
 const error = require('../../api/error.js')
+const mongoose = require('mongoose')
+
 
 module.exports = class warn extends Command {
 	constructor(client) {
