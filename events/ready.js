82cd0d83a3d3d27ffba9f0e86e059701cf5fb86a
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2021-02-26 23:24:21 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,8 @@ const Event = require('../structures/EventManager');
 
 module.exports = class extends Event {
 
-	constructor(...args) {
-		super(...args, {
-			once: true
-		});
+	constructor(client) {
+		this.client = client
 	}
 
 	async run() {
@@ -17,6 +15,20 @@ module.exports = class extends Event {
 			`${this.client.commands.size} comandos carregados!`,
 			`${this.client.events.size} eventos carregados!`
 		].join('\n'));
+		
+		let status [ 	
+			{name: `Pó de café na pia`, type: 'PLAYING'},
+			{name: 'Me mencione para saber mais sobre mim!', type: 'PLAYING'},
+			{name: `${this.client.guilds.size} universos diferentes!`, type: 'WATCHING'},
+			{name: 'Observou bugs? Tem sugestões ou dúvidas? Envie uma DM ao meu criador: Near#7447', type: 'PLAYING'},
+			{name: 'Servidor de suporte em andamento.', type: 'PLAYING'}
+		]
+		
+		setInterval(() => {
+			let randomStatus = status[Math.floor(Math.random() * status.length)]
+			this.client.user.setPresence({ activity: randomStatus })
+		}, 30000)
+		
 	}
   
 };
