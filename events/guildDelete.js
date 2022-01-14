cd68bfb6e1eb70d5168ee808d98292983734c033
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2022-01-14 01:37:17 -0400

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -11,7 +11,7 @@ module.exports = class GuildDelete {
 	    
 	    if(!server) return
 	    
-            let dono = guild.owner.id
+            let dono = guild.owner
 
             const leaveMessage = new Discord.MessageEmbed()
             .setColor('RANDOM')
