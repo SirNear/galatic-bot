6e425d502e092c33cd57563baeefc187284c679b
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2022-01-14 13:08:32 -0400

    Update dataPlayers.js

--- dbrpg/dataPlayers.js
+++ dbrpg/dataPlayers.js
@@ -3,3 +3,28 @@ const mongoose = require('mongoose')
 mongoose.connect(config.dbrpgplay, { useNewUrlParser: true}, (err) => { //Vamos fazer o bot se conectar no banco de dados
   if (err) console.error(`Não foi possível se conectar ao banco de dados ${err}`) //Caso dê algum erro, Whoops.
 })
+
+let ficha = new mongoose.Schema({
+  _id: {type: String},
+  username: {type: String},
+  idade: {type: String},
+  região: {type: String},
+  time: {type: String},
+  aparencia: {type: String},
+  isPoke: {type: Boolean, default: no},
+  pokeType: {type: String},
+  pokeName: {type: String},
+  pokeMoves: {type: String}
+})
+
+let pokedex = new mongoose.Schema({
+  _id: {type: String},
+  pokemons: {type: Map, of: String}
+})
+
+let fichaUser = mongoose.model("fichaUser", ficha)
+module.exports.fichaUser = fichaUser
+
+let pokeDex = mongoose.model("pokeDex", pokedex)
+module.exports.pokeDex = pokeDex
+
