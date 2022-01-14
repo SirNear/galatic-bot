13232ec1eaf11e994517f35fb5ba23745b0d3e56
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2022-01-13 20:53:35 -0400

    Create guildCreate.js

---
+++ events/guildCreate.js
@@ -0,0 +1,21 @@
+module.exports = class GuildCreate = {
+  constructor(client) {
+    this.client = client
+  }
+
+  async run(guild) {
+    
+    const server = await this.client.database.Guilds.findById(guild.id)
+    
+    if (!server) {
+      this.client.database.Guilds({
+          _id: guild.id
+      }).save().then(msg =>{
+          console.log('Deu certo bro')
+      })
+    }
+    
+    
+  }
+
+}
