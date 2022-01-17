3106596e3f02ec5fdd8eb71cba166f6f9edad8e5
Henrique Batista <62736816+SirNear@users.noreply.github.com>
2022-01-17 17:31:42 -0400

    Update ficha.js

--- commands/rpg/ficha.js
+++ commands/rpg/ficha.js
@@ -35,6 +35,7 @@ async run({ message, args, client, server, dataPlayer}){
 			    const filtroName = m => m.author.id === message.author.id
               		    const collectorNome = msg.channel.awaitMessages(filtroName, { idle: 10000}).then(collected => {
 				    let charName = collected.first();
+				    console.log(charName)
 				    dataPlayer.nome = charName
 				    dataPlayer.save()
 				    
