ea1742c17ac1a8e084c719e84753380f846638fe
Henrique Batista <offhenriquebj@gmail.com>
2023-03-11 16:18:43 -0400

    Tentativa de erro de execução: unhandled promise rejection

--- events/message.js
+++ events/message.js
@@ -66,11 +66,17 @@ module.exports = class MessageReceive {
 		const command = args.shift().toLowerCase()
 		const comando = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command))
     
-    try {
-      new Promise((res, rej) => {
-				message.channel.startTyping()
-				res(comando.run({ message, args, server }))
-			}).then(() => message.channel.stopTyping())
+	try {
+	  new Promise((res, rej) => {
+	    message.channel.startTyping()
+	    res(comando.run({ message, args, server }))
+	  })
+	    .then(() => message.channel.stopTyping())
+	    .catch((err) => console.error(err))
+	} catch (err) {
+	  console.error(err)
+	}
+
 	    
 	          const bt = message.guild.member(message.guild.members.cache.get(this.client.user.id))
 
