b0fb106f4bc69daa4cc0ff8dbb0af08363e469ff
Henrique Batista <offhenriquebj@gmail.com>
2023-03-11 16:51:05 -0400

    Update GalaticClient.js

--- GalaticClient.js
+++ GalaticClient.js
@@ -1,6 +1,5 @@
 const Util = require('./structures/Util.js')
-const { Collection, Discord, Intents } = require("discord.js")
-const { Client } = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const { Client, Collection, Discord } = require("discord.js")
 const { readdir } = require("fs")
 const config = require('./config.json')
 const EventManager = require('./structures/EventManager.js')
