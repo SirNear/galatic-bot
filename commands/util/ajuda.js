dd7aaaa88f6b74a4558e1df1d791ca4c954b26f8
Henrique Batista <offhenriquebj@gmail.com>
2023-03-12 19:14:45 -0400

    Atualização fields, unificados agora

--- commands/util/ajuda.js
+++ commands/util/ajuda.js
@@ -21,12 +21,13 @@ module.exports = class ajuda extends Command {
     .setTimestamp()
     .setThumbnail(this.client.user.displayAvatarURL())
     .setAuthor(`${message.guild.name} | Menu de Ajuda`, message.guild.iconURL({ dynamic: true }))	       
-    .addFields(`<:7079_rimuru_slime_shocked:810735162286407742> Utilitário | (${this.getCommmandSize("util")})`, this.getCategory("util", server.prefix))
-    .addFields(`<:3223_ComfyAmongus:810735118615314443> Desenvolvedor | (${this.getCommmandSize("dev")})`, this.getCategory("dev", server.prefix))
-    .addFields(`<:7992_AmongUs_Investigate:810735122462670869> Moderação | (${this.getCommmandSize("moderation")})`, this.getCategory("moderation", server.prefix))
-    .addFields(`<:PeepoPing:810735232918487091> Configuração do Servidor | (${this.getCommmandSize("config")})`, this.getCategory("config", server.prefix))
-    .addFields(`**Convite do Bot**`, "[CLIQUE AQUI](https://discord.com/oauth2/authorize?client_id=INSERT_CLIENT_ID_HERE&scope=bot&permissions=8)")
-
+    .addFields(
+	    {name: `<:7079_rimuru_slime_shocked:810735162286407742> Utilitário | (${this.getCommmandSize("util")})`, value: this.getCategory("util", server.prefix)},
+	    {name: `<:3223_ComfyAmongus:810735118615314443> Desenvolvedor | (${this.getCommmandSize("dev")})`, value: this.getCategory("dev", server.prefix)},
+	    {name: `<:7992_AmongUs_Investigate:810735122462670869> Moderação | (${this.getCommmandSize("moderation")})`, value: this.getCategory("moderation", server.prefix)},
+   	    {name: `<:PeepoPing:810735232918487091> Configuração do Servidor | (${this.getCommmandSize("config")})`, value: this.getCategory("config", server.prefix)},
+	    {name: `**Convite do Bot**`, value: "[CLIQUE AQUI](https://discord.com/oauth2/authorize?client_id=INSERT_CLIENT_ID_HERE&scope=bot&permissions=8)")}
+     )
      message.author.send(embed).then(() => {
 	message.reply('**Verifique sua DM**')
 }).catch(() => {
