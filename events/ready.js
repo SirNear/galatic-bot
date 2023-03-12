7a5a5e14bd68b275e56d2030d72ae7fb90728617
Henrique Batista <offhenriquebj@gmail.com>
2023-03-12 15:39:00 -0400

    Atualização de código das atividades do bot
    Antes apenas "type: playing, watching, etc." agora é "ActivityType."

--- events/ready.js
+++ events/ready.js
@@ -58,11 +58,11 @@ module.exports = class {
 		].join('\n'));
 		
 		let status = [ 	
-			{name: `Pó de café na pia`, type: 'PLAYING'},
-			{name: 'Me mencione para saber mais sobre mim!', type: 'PLAYING'},
-			{name: `${this.client.guilds.size} universos diferentes!`, type: 'WATCHING'},
-			{name: 'Observou bugs? Tem sugestões ou dúvidas? Envie uma DM ao meu criador: Near#7447', type: 'PLAYING'},
-			{name: 'Servidor de suporte em andamento.', type: 'PLAYING'}
+			{name: `Pó de café na pia`, type: ActivityType.Playing},
+			{name: 'Me mencione para saber mais sobre mim!', type: ActivityType.Playing},
+			{name: `${this.client.guilds.size} universos diferentes!`, type: ActivityType.Watching},
+			{name: 'Observou bugs? Tem sugestões ou dúvidas? Envie uma DM ao meu criador: Near#7447', type: ActivityType.Playing},
+			{name: 'Servidor de suporte em andamento.', type: ActivityType.Playing}
 		]
 		
 		setInterval(() => {
