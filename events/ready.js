6ef0df1a84c181e70bba21a2d290029b2024c337
Henrique Batista <offhenriquebj@gmail.com>
2023-03-12 15:42:16 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -69,7 +69,7 @@ module.exports = class {
 		
 		setInterval(() => {
 			let randomStatus = status[Math.floor(Math.random() * status.length)]
-			this.client.user.setActivity(randomStatus)
+			this.client.user.setPresence({ activities: randomStatus)
 		}, 30000)
 		
 	}
