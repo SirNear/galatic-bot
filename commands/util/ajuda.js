e5e7de9ef853c63860d139bb1922c53ce2d4d7fd
Henrique Batista <offhenriquebj@gmail.com>
2023-03-12 19:09:05 -0400

    Atualização embed

--- commands/util/ajuda.js
+++ commands/util/ajuda.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const Command = require('../../structures/Command');
 
 module.exports = class ajuda extends Command {
@@ -16,16 +16,16 @@ module.exports = class ajuda extends Command {
   
 	 run({ message, args, client, server}) {
       		
-    const embed = new MessageEmbed() 
+    const embed = new EmbedBuilder() 
     .setColor('#bc42f4')
     .setTimestamp()
     .setThumbnail(this.client.user.displayAvatarURL())
     .setAuthor(`${message.guild.name} | Menu de Ajuda`, message.guild.iconURL({ dynamic: true }))	       
-    .addField(`<:7079_rimuru_slime_shocked:810735162286407742> Utilitário | (${this.getCommmandSize("util")})`, this.getCategory("util", server.prefix))
-    .addField(`<:3223_ComfyAmongus:810735118615314443> Desenvolvedor | (${this.getCommmandSize("dev")})`, this.getCategory("dev", server.prefix))
-    .addField(`<:7992_AmongUs_Investigate:810735122462670869> Moderação | (${this.getCommmandSize("moderation")})`, this.getCategory("moderation", server.prefix))
-    .addField(`<:PeepoPing:810735232918487091> Configuração do Servidor | (${this.getCommmandSize("config")})`, this.getCategory("config", server.prefix))
-    .addField(`**Convite do Bot**`, "[CLIQUE AQUI](https://discord.com/oauth2/authorize?client_id=INSERT_CLIENT_ID_HERE&scope=bot&permissions=8)")
+    .addFields(`<:7079_rimuru_slime_shocked:810735162286407742> Utilitário | (${this.getCommmandSize("util")})`, this.getCategory("util", server.prefix))
+    .addFields(`<:3223_ComfyAmongus:810735118615314443> Desenvolvedor | (${this.getCommmandSize("dev")})`, this.getCategory("dev", server.prefix))
+    .addFields(`<:7992_AmongUs_Investigate:810735122462670869> Moderação | (${this.getCommmandSize("moderation")})`, this.getCategory("moderation", server.prefix))
+    .addFields(`<:PeepoPing:810735232918487091> Configuração do Servidor | (${this.getCommmandSize("config")})`, this.getCategory("config", server.prefix))
+    .addFields(`**Convite do Bot**`, "[CLIQUE AQUI](https://discord.com/oauth2/authorize?client_id=INSERT_CLIENT_ID_HERE&scope=bot&permissions=8)")
 
      message.author.send(embed).then(() => {
 	message.reply('**Verifique sua DM**')
