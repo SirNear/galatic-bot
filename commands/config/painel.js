c3e4f2e3b354bf8afe9abe5fc9c911659db3d34b
Henrique Batista <offhenriquebj@gmail.com>
2023-03-19 16:52:22 -0300

    Update painel.js

--- commands/config/painel.js
+++ commands/config/painel.js
@@ -83,7 +83,7 @@ module.exports = class painel extends Command {
 		    collector.on("collect", (interaction) => {
 		    	switch(interaction.customId) {
 				case "1":
-					message.channel.send('***Mencione o novo canal de punições***')
+					interaction.reply({content: '***Mencione o novo canal de punições***', components: [row[0]]})
 					const filter1 = (m) => m.author.id === message.author.id;
 					const collector1 = new MessageCollector(message.channel, filter, {time: 2000})
 					collector1.on("collect", (collected) => {
