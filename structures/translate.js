21fef481186c0830edeac62d9168327f89688b48
Henrique Batista <offhenriquebj@gmail.com>
2023-03-20 20:44:35 -0400

    Create translate.js

---
+++ structures/translate.js
@@ -0,0 +1,26 @@
+const translate = require('google-translate-api');
+
+module.exports = class translate {
+  construtor(client) {
+    this.client = client;
+  
+  }
+
+async function translateWord(word, sourceLang, targetLang) {
+  try {
+    const result = await translate(word, { from: sourceLang, to: targetLang });
+    return result.text;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const word = 'hello';
+const sourceLang = 'en';
+const targetLang = 'pt';
+
+translateWord(word, sourceLang, targetLang)
+  .then(translatedWord => console.log(translatedWord))
+  .catch(error => console.error(error));
+
+}
