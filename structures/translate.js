ddba422ef7427dd16f48b82300ed792e45777033
Henrique Batista <offhenriquebj@gmail.com>
2023-03-20 21:25:07 -0400

    Update translate.js

--- structures/translate.js
+++ structures/translate.js
@@ -9,13 +9,14 @@ async function translateWord(word, sourceLang, targetLang) {
   }
 }
 
-const word = 'hello';
-const sourceLang = 'en';
-const targetLang = 'pt';
-
-translateWord(word, sourceLang, targetLang)
-  .then(translatedWord => console.log(translatedWord))
-  .catch(error => console.error(error));
+async function translateWord(word, sourceLang, targetLang) {
+  try {
+    const result = await translate(word, { from: sourceLang, to: targetLang });
+    return result.text;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 
 module.exports = class translate {
