969dc06a3041c92954a3a4ad8d3fd61692d79d25
Henrique Batista <offhenriquebj@gmail.com>
2023-03-25 20:40:17 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -100,7 +100,7 @@ module.exports = class MessageReceive {
 
 			const pokeMsg = await message.channel.send({ embeds: [embed] })
 
-			const collector = await pokeMsg.channel.createMessageCollector({(m) => m.author.id === msg.author.id, time: 120000, max: 1})
+			const collector = await pokeMsg.channel.createMessageCollector({ filter: (m) => m.author.id === msg.author.id, time: 120000, max: 1})
 			collector.on('collect', async (collected) => {
 				if(collected.first().content === 'g!capturar') {
 					msg.reply({content: 'testando'})
