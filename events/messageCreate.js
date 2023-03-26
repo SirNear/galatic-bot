bcf219db1b6cd82d540438650dd4bc228af320ff
Henrique Batista <offhenriquebj@gmail.com>
2023-03-25 21:57:20 -0400

    Correção de sintaxes

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -55,94 +55,76 @@ module.exports = class MessageReceive {
 				}//if g!capturar
 			})//collector
 
-		}else{ return }
+		}
 		
 		//pokemon
 
-	 if (!server) {
-      this.client.database.Guilds({
-          _id: message.guild.id
-      }).save().then(msg =>{
-          console.log('Deu certo bro')
-      })
-    }
+		 if (!server) { //Se a guilda não tiver salva
+		      this.client.database.Guilds({
+			  _id: message.guild.id
+		      }).save().then(msg =>{
+			  console.log('Deu certo bro')
+		      })
+		    }
 		
-	let userDb = await this.client.database.userData.findById(message.author.id)
+		let userDb = await this.client.database.userData.findById(message.author.id)
 
 		
-         if(!userDb) {
-              this.client.database.userData({
-                 _id: message.author.id,
-                  uid: message.author.id,
-                  uName: message.author.username,
-                 uServer: message.guild.id,
-             }).save()
+		 if(!userDb) {//se o usuário não estiver salvo
+		      this.client.database.userData({
+			 _id: message.author.id,
+			  uid: message.author.id,
+			  uName: message.author.username,
+			 uServer: message.guild.id,
+		     }).save()
+		 }
 
-         }
-         
-  
-     if(userDb.monitor == 'ativado') {
+	          if(userDb.monitor == 'ativado') {// se o monitoramento do usuario estiver ativo
+		          if(!message.guild.channels.cache.get(`${userDb.monitorChannelId}`)) {//se não tiver canal de monitoramento
+				  userDb.monitor = 'desativado'
+				  userDb.save()
 
-        if(!message.guild.channels.cache.get(`${userDb.monitorChannelId}`)) {
-         userDb.monitor = 'desativado'
-         userDb.save()
-		
+		 	  }else { //se tiver canal de monitoramento
+				  let embedMonitor = new EmbedBuilder()
+				      .setTitle(`<:nani:572425789178642472> | **Nova mensagem de ${message.author.username}** | <:nani:572425789178642472>`)
+				      .setDescription(message.content)
+				      .addFields({name: `**Canal:**`, value: message.channel})
+				      .setTimestamp()
 
-    }else {
+				  message.guild.channels.cache.get(`${userDb.monitorChannelId}`).send({embeds: [embedMonitor]})
 
-
-      let embedMonitor = new Discord.MessageEmbed()
-      .setTitle(`<:nani:572425789178642472> | **Nova mensagem de ${message.author.username}** | <:nani:572425789178642472>`)
-      .setDescription(message.content)
-      .addField(`**Canal:**`, message.channel)
-      .setTimestamp()
-
-
-       message.guild.channels.cache.get(`${userDb.monitorChannelId}`).send(embedMonitor)
-
-    }       
-   }
-     
-
-     
-   
-	if (!message.content.startsWith(server.prefix)) return
+			}//else
+		  }//if se monitorar
+ 
+		if (!message.content.startsWith(server.prefix)) return; //se não começar com o prefixo
 		const args = message.content.slice(server.prefix.length).trim().split(/ +/g)
 		const command = args.shift().toLowerCase()
 		const comando = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command))
-    
-    try {
-      new Promise((res, rej) => {
-		message.channel.sendTyping()
-		res(comando.run({ message, args, server }))
-	})
-	    
-	          const bt = message.guild.members.cache.get(this.client.user.id)
-
-          	   if(!bt.permissions.has("ADMINISTRATOR")) {
-			   
-      
-              		  let dono = message.guild.owner
-      
-           	         const embed = new EmbedBuilder()
-             		   .setColor('RANDOM')
-               		   .setTitle('<:error_2:676239899065843722> | Sem Permissão | <:error_2:676239899065843722>')
-                	   .setDescription(`Olá ${dono.user}, estou no seu servidor ${message.guild.name} porém meu cargo está sem a permissão \`ADMINISTRADOR\` e preciso dela para funcionar.`)
-      
-                	 dono.send(embed)
-        
-		  }
-    		} catch (err) {
-				
+   
+		try {
 			
-		message.channel.send(`**ERRO:**\`${err}\``)
-		console.error(err.stack)
-				
-		}
-		
-		// Pokemon
-		
+		        new Promise((res, rej) => {
+				message.channel.sendTyping()
+				res(comando.run({ message, args, server }))
+			})
+	    
+		       const bt = message.guild.members.cache.get(this.client.user.id)
 
+			if(!bt.permissions.has("ADMINISTRATOR")) {
+				  let dono = message.guild.owner
+      
+				 const embed = new EmbedBuilder()
+				   .setColor('RANDOM')
+				   .setTitle('<:error_2:676239899065843722> | Sem Permissão | <:error_2:676239899065843722>')
+				   .setDescription(`Olá ${dono.user}, estou no seu servidor ${message.guild.name} porém meu cargo está sem a permissão \`ADMINISTRADOR\` e preciso dela para funcionar.`)
+
+				 dono.send(embed)
+        
+		       }//if bt
+    		} catch (err) {
+			message.channel.send(`**ERRO:**\`${err}\``)
+			console.error(err.stack)	
+		}//error try
 		
 		
 	    }
