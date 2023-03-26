d511f9566aef9047f7e7c82b0ad5f8353a4674d7
Henrique Batista <offhenriquebj@gmail.com>
2023-03-25 23:00:28 -0400

    Create tiposPokemon.js

---
+++ api/tiposPokemon.js
@@ -0,0 +1,23 @@
+const tiposPokemon = {
+  fire: ["vulcao", "vulcão", "praia", "ilha", "deserto", "lava", "magma"],
+  ground: ["floresta", "praia", "montanha", "deserto", "campo", "caverna"],
+  water: ["mar", "oceano", "rio", "lago", "bacia", "pantano", "pantâno", "manguezal", "praia", "ilha", "cachoeira"],
+  grass: ["floresta", "campo", "mata", "matagal"],
+  dark: ["escuro", "escura", "caverna", "beco"],
+  ghost: ["casa abandonada", "ruina", "cemiterio", "cemitério", "tumulo", "túmulo"],
+  eletric: ["caverna", "cidade", "vila", "aldeia", "cidadãos", "cidadaos", "pessoas", "moradores"],
+  ice: ["montanha", "neve", "gelo", "nevasca", "nevada", "frio", "fria"],
+  fighting: ["ginasio", "ginásio", "pessoas", "cidade", "vila", "aldeia", "cidadãos", "cidadaos", "moradores", "floresta", "cachoeira"],
+  poison: ["floresta", "caverna", "mar", "oceano"],
+  psychic: ["floresta densa", "templo", "ruina", "sagrado", "espiritual", "montanha", "ilha"],
+  bug: ["floresta", "campo", "arvore", "árvore", "árvores", "arvores", "caverna", "casas", "casa"],
+  rock: ["montanha", "caverna", "pedras", "pedregoso", "planalto", "praia"],
+  dragon: ["montanha", "floresta densa", "picos"],
+  steel: ["caverna", "montanha", "mina"],
+  fairy: ["sagrado", "floresta", "templo", "igreja", "espiritual", "ilha", "montanha", "caverna"]
+ 
+}
+
+module.exports = { tiposPokemon }
+         
+         //fire = chave/tipo; [] = array/palavras-chave
