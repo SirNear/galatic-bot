d25abda8ee2d4d585107687a5cb39cd79bda1cb8
Henrique Batista <offhenriquebj@gmail.com>
2023-03-26 20:40:48 -0300

    Update typeIdentifier.js

--- api/typeIdentifier.js
+++ api/typeIdentifier.js
@@ -1,22 +1,62 @@
-const types = {
-  'normal': 1,
-  'fighting': 2,
-  'flying': 3,
-  'poison': 4,
-  'ground': 5,
-  'rock': 6,
-  'bug': 7,
-  'ghost': 8,
-  'steel': 9,
-  'fire': 10,
-  'water': 11,
-  'grass': 12,
-  'electric': 13,
-  'psychic': 14,
-  'ice': 15,
-  'dragon': 16,
-  'dark': 17,
-  'fairy': 18
-};
+			function handleTypeIdentifier(type) {
+			  switch (type) {
+			    case 'fire':
+			      return 10;
+			      break;
+			    case 'water':
+			      return 11;
+			      break;
+			    case 'grass':
+			      return 12;
+			      break;
+			    case 'electric':
+			      return 13;
+			      break;
+			    case 'rock':
+			      return 6;
+			      break;
+			    case 'ground':
+			      return 5;
+			      break;
+			    case 'psychic':
+			      return 14;
+			      break;
+			    case 'ice':
+			      return 15;
+			      break;
+			    case 'fighting':
+			      return 2;
+			      break;
+			    case 'ghost':
+			      return 8;
+			      break;
+			    case 'poison':
+			      return 4;
+			      break;
+			    case 'flying':
+			      return 3;
+			      break;
+			    case 'bug':
+			      return 7;
+			      break;
+			    case 'dark':
+			      return 17;
+			      break;
+			    case 'steel':
+			      return 9;
+			      break;
+			    case 'dragon':
+			      return 16;
+			      break;
+			    case 'fairy':
+			      return 18;
+			      break;
+          case 'normal':
+            return 1
+			    default:
+			      return type;
+			      break;
+			  }
+			}
 
-module.exports = types;
+module.exports = { handlePokemonType }
