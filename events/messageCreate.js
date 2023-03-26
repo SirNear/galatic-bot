c276daa5f8bc0d92891fc45aaf2c581666612590
Henrique Batista <offhenriquebj@gmail.com>
2023-03-25 21:35:54 -0400

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -19,67 +19,6 @@ module.exports = class MessageReceive {
 		const random = Math.random()
 		
 		if(random < chance) {
-		
-			function handlePokemonType(type) {
-			  switch (type) {
-			    case 'fire':
-			      return 'fogo';
-			      break;
-			    case 'water':
-			      return 'água';
-			      break;
-			    case 'grass':
-			      return 'grama';
-			      break;
-			    case 'electric':
-			      return 'elétrico';
-			      break;
-			    case 'rock':
-			      return 'pedra';
-			      break;
-			    case 'ground':
-			      return 'terra';
-			      break;
-			    case 'psychic':
-			      return 'psíquico';
-			      break;
-			    case 'ice':
-			      return 'gelo';
-			      break;
-			    case 'fighting':
-			      return 'lutador';
-			      break;
-			    case 'ghost':
-			      return 'fantasma';
-			      break;
-			    case 'poison':
-			      return 'veneno';
-			      break;
-			    case 'flying':
-			      return 'voador';
-			      break;
-			    case 'bug':
-			      return 'inseto';
-			      break;
-			    case 'dark':
-			      return 'sombrio';
-			      break;
-			    case 'steel':
-			      return 'aço';
-			      break;
-			    case 'dragon':
-			      return 'dragão';
-			      break;
-			    case 'fairy':
-			      return 'fada';
-			      break;
-			    default:
-			      return type;
-			      break;
-			  }
-			}
-
-
 
 			const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
 			const data = await response.json();
