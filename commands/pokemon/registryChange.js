e748033925d19dec1dac404667288f786618878d
Henrique Batista <offhenriquebj@gmail.com>
2023-03-26 22:06:32 -0300

    Create registryChange.js

---
+++ commands/pokemon/registryChange.js
@@ -0,0 +1,52 @@
+
+const { Discord, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
+const Command = require('../../structures/Command');
+const error = require('../../api/error.js')
+
+module.exports = class registryChange extends Command {
+	constructor(client) {
+		super(client, {
+			name: "registryChange",
+			category: "pokemon",
+			aliases: ['rc'],
+			UserPermission: ["ADMINISTRATOR"],
+			clientPermission: null,
+			OnlyDevs: false
+		})
+	}
+  
+async run({ message, args, client, server}) {
+  
+  let pokeReg = await this.client.database.pokeReg.findOne({ pokeName: args.slice(0).join(' ') })
+  if(!pokeReg) {
+    
+     const msgNoPoke = await message.reply({ content: 'Esse pokémon não foi registrado, deseja registrar?', ephemeral: true, components: [row] });
+    
+     const row = new ActionRowBuilder()
+     .addComponents(
+       new ButtonBuilder()
+       .setCustomId('primary')
+       .setLabel('SIM')
+       .setStyle(ButtonStyle.Success),
+       new ButtonBuilder()
+       .setCustomId('secondary')
+       .setLabel('NÃO')
+       .setStyle(ButtonStyle.Danger),
+     );
+    
+    const filterSim = i => i.customId === 'primary' && i.user.id === message.author.id;
+    const collectorSim = pokeMsg.channel.createMessageComponentCollector({ filterSim, time: 15000 })
+    
+    const filterNao = i => i.customId === 'secondary' && i.user.id === message.author.id;
+    const collectorNao = pokeMsg.channel.createMessageComponentCollector({ filterNao, time: 15000 })
+    
+    collectorSim.on('collect', (collected) => {
+      message.reply({content: 'a'})
+    })
+    
+  }//if pokeReg
+  
+  
+  
+  }
+}
