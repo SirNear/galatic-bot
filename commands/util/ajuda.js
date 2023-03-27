15bf1a343cf4a6c8893d54bcebb635497eedcb39
Henrique Batista <offhenriquebj@gmail.com>
2023-03-26 22:08:21 -0300

    ADição categoria "pokemon"

--- commands/util/ajuda.js
+++ commands/util/ajuda.js
@@ -26,6 +26,7 @@ module.exports = class ajuda extends Command {
 	    {name: `<:3223_ComfyAmongus:810735118615314443> Desenvolvedor | (${this.getCommmandSize("dev")})`, value: this.getCategory("dev", server.prefix)},
 	    {name: `<:7992_AmongUs_Investigate:810735122462670869> Moderação | (${this.getCommmandSize("moderation")})`, value: this.getCategory("moderation", server.prefix)},
    	    {name: `<:PeepoPing:810735232918487091> Configuração do Servidor | (${this.getCommmandSize("config")})`, value: this.getCategory("config", server.prefix)},
+	    {name: `<:tilapia:811639469199065148> Pokémon | (${this.getCommmandSize("pokemon")})`, value: this.getCategory("pokemon", server.prefix)},
 	    {name: `**Convite do Bot**`, value: "[CLIQUE AQUI](https://discord.com/oauth2/authorize?client_id=INSERT_CLIENT_ID_HERE&scope=bot&permissions=8)"}
      )
      this.client.users.send(message.author.id, {embeds: [embed] }).then(() => {
