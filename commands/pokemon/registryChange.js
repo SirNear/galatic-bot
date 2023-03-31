03ce2cfb4090c85e8930dcca9565d2345b06a9b8
Henrique Batista <offhenriquebj@gmail.com>
2023-03-30 23:29:25 -0300

    Feita a parte de se registrar o pokémon

--- commands/pokemon/registryChange.js
+++ commands/pokemon/registryChange.js
@@ -1,7 +1,9 @@
 
-const { Discord, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
+const { Discord, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Events } = require('discord.js');
 const Command = require('../../structures/Command');
-const error = require('../../api/error.js')
+const color = require('../../api/colors.js')
+const error = require('../../api/error.json')
+const fetch = require('node-fetch')
 
 module.exports = class registryChange extends Command {
 	constructor(client) {
@@ -42,10 +44,74 @@ async run({ message, args, client, server}) {
 	    const collectorNao = msgNoPoke.channel.createMessageComponentCollector({ filterNao, time: 15000 })
 
 	    collectorSim.on('collect', (collected) => {
-	      message.reply({content: 'a'})
+		    message.delete()
+		    
+		    const embedReg = new EmbedBuilder()
+		    .setTitle('Registro de Pokémon')
+		    .setDescription('Qual é o nome do pokémon?')
+		    .setColor(color.purple)
+		    
+		    const pokeMsg = await message.channel.send({embeds: [embedReg]})
+		    const collectorNome = await pokeMsg.channel.createMessageCollector({ filter: (m) => m.author.id === message.author.id, time: 120000, max: 1})
+		    collectorNome.on("collect", (collected) => {
+			    let regName = collected.content.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+			    
+			    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${regName}`)
+			    let data = await response.json();
+			    let regId = data.id
+			    
+			    if(!regId) {
+				    pokeMsg.delete()
+				    message.reply({content: '**Esse pokémon não existe! Por favor, repita o comando.**'})
+			    }
+			    
+			    pokeMsg.delete()
+			    embedReg.setDescription('Qual é a descrição do pokémon?')
+			    embedReg.addFields({name: '**Nome**', value: regName})
+			    
+			    const nomeMsg = await message.channel.send({embeds: [embedReg]})
+			    const collectorDesc = await nomeMsg.channel.createMessageCollector({ filter: (m) => m.author.id === message.author.id, time: 120000, max: 1})
+			    collectorDesc.on("collect", (collected) => {
+				    nomeMsg.delete()
+				    let regDesc = collected.content
+				    embedReg.setDescription('Qual é o tipo do pokémon? Em inglês. Se for mais de um tipo, separe por uma "/"')
+				    embedReg.addFields({name: '**Descrição**', value: regDesc})
+				    
+				    const descMsg = await message.channel.send({embeds: [embedReg]})
+				    const collectorType = descMsg.channel.createMessageCollector({ filter: (m) => m.author.id === message.author.id, time: 120000, max: 1})
+				    collectorType.on("collect", (collected) => {
+					    descMsg.delete()
+					    let regType = collected.content
+					    embedReg.setDescription('Qual é a espécie do pokémon? Ex.: Lucario é da espécie pokémon aura.')
+					    embedReg.addFields({name: '**Tipos:**', value: regType})
+					    
+					    const typeMsg = await message.channel.send({embeds: [embedReg]})
+					    const collectorTitle = typeMsg.channel.createMessageCollector({ filter: (m) => m.author.id === message.author.id, time: 120000, max: 1})
+					    collectorTitle.on('collect', (collected) => {
+						    typeMsg.delete()
+						    let regTitle = collected.content
+						    embedReg.setDescription('Registro concluido!')
+						    embedReg.addFields({name: '**Espécie:**', value: regTitle})
+						    
+						    message.channel.send({embeds: [embedReg]}).then(msg => {
+							    this.client.database.pokeReg({
+								    _id: regId,
+								    pokeName: regName,
+								    pokeDesc: regDesc,
+								    pokeType: regType,
+								    pokeTitle: regTitle
+							    }).save().then(msg => {
+								    console.log('Novo pokémon registrado:' + ` ${regName} / ${regType}`)
+								    message.channel.send({embeds: [embedReg]})
+							    })//then save database
+						    })//then embed
+					    })//collectorTitle
+				    })//collectorType
+			    })//collectorDesc
+		    })//collectorNome
 	    })
     
-  }//if pokeReg
+  }//if !pokeReg
   
   
   
