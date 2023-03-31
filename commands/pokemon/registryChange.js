27cd1a6b417630f42823f322d2191900871abbac
Henrique Batista <offhenriquebj@gmail.com>
2023-03-30 21:54:14 -0300

    Update registryChange.js

--- commands/pokemon/registryChange.js
+++ commands/pokemon/registryChange.js
@@ -19,30 +19,31 @@ async run({ message, args, client, server}) {
   
   let pokeReg = await this.client.database.pokeReg.findOne({ pokeName: args.slice(0).join(' ') })
   if(!pokeReg) {
+	  
+	  const row = new ActionRowBuilder()
+     		.addComponents(
+       			new ButtonBuilder()
+       			.setCustomId('primary')
+		       .setLabel('SIM')
+		       .setStyle(ButtonStyle.Success),
+		       new ButtonBuilder()
+		       .setCustomId('secondary')
+		       .setLabel('NÃO')
+		       .setStyle(ButtonStyle.Danger),
+		     );
+	  
     
-     const msgNoPoke = await message.reply({ content: 'Esse pokémon não foi registrado, deseja registrar?', ephemeral: true, components: [row] });
-    
-     const row = new ActionRowBuilder()
-     .addComponents(
-       new ButtonBuilder()
-       .setCustomId('primary')
-       .setLabel('SIM')
-       .setStyle(ButtonStyle.Success),
-       new ButtonBuilder()
-       .setCustomId('secondary')
-       .setLabel('NÃO')
-       .setStyle(ButtonStyle.Danger),
-     );
-    
-    const filterSim = i => i.customId === 'primary' && i.user.id === message.author.id;
-    const collectorSim = pokeMsg.channel.createMessageComponentCollector({ filterSim, time: 15000 })
-    
-    const filterNao = i => i.customId === 'secondary' && i.user.id === message.author.id;
-    const collectorNao = pokeMsg.channel.createMessageComponentCollector({ filterNao, time: 15000 })
-    
-    collectorSim.on('collect', (collected) => {
-      message.reply({content: 'a'})
-    })
+	    const msgNoPoke = await message.reply({ content: 'Esse pokémon não foi registrado, deseja registrar?', ephemeral: true, components: [row] });
+
+	    const filterSim = i => i.customId === 'primary' && i.user.id === message.author.id;
+	    const collectorSim = pokeMsg.channel.createMessageComponentCollector({ filterSim, time: 15000 })
+
+	    const filterNao = i => i.customId === 'secondary' && i.user.id === message.author.id;
+	    const collectorNao = pokeMsg.channel.createMessageComponentCollector({ filterNao, time: 15000 })
+
+	    collectorSim.on('collect', (collected) => {
+	      message.reply({content: 'a'})
+	    })
     
   }//if pokeReg
   
