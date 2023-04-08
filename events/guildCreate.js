23926d702962bec979d8effd01cba55cf0699457
Henrique Batista <offhenriquebj@gmail.com>
2023-04-07 21:28:40 -0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,16 +1,16 @@
 const { EmbedBuilder } = require('discord.js')
 
-module.exports = class {
+module.exports = class GuildCreate {
 
 	constructor(client) {
 		this.client = client
 	}
   
   async run(client, guild) {
-    const server = await this.client.database.Guilds.findById(this.client.guild.id);
+    const server = await this.client.database.Guilds.findById(guild.id);
     
     if(!server) {
-      console.log(this.client.guild.id + this.client.guild.name)
+      console.log(guild.id + guild.name)
       //this.client.database.Guilds({
       //  _id: guild.name,
       //}).save().then(msg => {
