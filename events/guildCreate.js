553d28ba139cb3abab2f322919ed1c9f0a2a9513
Henrique Batista <offhenriquebj@gmail.com>
2023-04-07 22:36:51 -0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -21,6 +21,21 @@ module.exports = class GuildCreate {
 		.setTitle('<a:hypeneon:729338461454205059> | **Você me adicionou ao seu servidor! = )**')
 		.setDescription(`Você me adicionou ao ${guild.name}, fico feliz por voltar! Verifique as configurações anteriores em **${server.prefix}painel ver**.`)
 		.setThumbnail(guild.icon);
+		
+		const leave = guild.leave()
+		
+		let embedCreated = new EmbedBuilder()
+		.setColor(color.green)
+		.setTitle('**NOVO SERVIDOR**')
+		.addFields(
+			{name: '**Servidor:**', value: guild.name},
+			{name: '**ID**', value: guild.id},
+			{name: '**Dono**', value: guild.ownerID},
+			{name: '**Sair do Servidor**', value: `[CLIQUE](${leave})`}
+		)
+		
+		const canal = client.channels.cache.get('1094070734151766026')
+		canal.send({ embeds: [embedCreated] })
 	  
 	  
 	  if(!server) {
