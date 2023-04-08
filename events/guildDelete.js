7770f305fb5e5e9e22be2eacecc8d2c42c66c25c
Henrique Batista <offhenriquebj@gmail.com>
2023-04-07 22:24:18 -0300

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -13,10 +13,10 @@ module.exports = class GuildDelete {
 		.setColor(color.green)
 		.setTitle('<:GadgetLeave:821521172956053513> | **Você me tirou do seu servidor! = (**')
 		.setDescription(`Você me expulsou do ${guild.name}, espero que eu não tenha feito nada de errado =( \n \n Entra em contato com o [suporte](https://discord.gg/EsAb4jDAvx) pra falar sobre sua insatisfação!`)
-    .addFields({name: `**Se quiser me adicionar novamente**`, value: "[CLIQUE AQUI](https://discord.com/oauth2/authorize?client_id=634216294710771713&scope=bot&permissions=8)"})
+   		 .addFields({name: `**Se quiser me adicionar novamente**`, value: "[CLIQUE AQUI](https://discord.com/oauth2/authorize?client_id=634216294710771713&scope=bot&permissions=8)"})
 		.setThumbnail(guild.icon);
     
-    client.users.send(guild.ownerId, {embeds: [embed]})
+    this.client.users.send(guild.ownerId, {embeds: [embed]})
 	  
    }
 }
