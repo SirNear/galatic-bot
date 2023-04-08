ad7110b823e8aa16d2e0962126eb1e4a75a3e453
Henrique Batista <offhenriquebj@gmail.com>
2023-04-07 21:50:06 -0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,20 +1,26 @@
 const { EmbedBuilder } = require('discord.js')
+const color = require('../../api/colors.json')
 
 module.exports = class GuildCreate {
 
 	constructor(client) {
 		this.client = client
 	}
-  
-  async run(guild) {
-    const server = await this.client.database.Guilds.findById(guild.id);
-    
-    if(!server) {
-      console.log(guild.id + guild.name)
-      //this.client.database.Guilds({
-      //  _id: guild.name,
-      //}).save().then(msg => {
-      //})
-    }
-  }
+
+	async run(guild) {
+		const server = await this.client.database.Guilds.findById(guild.id);
+		let embed = new EmbedBuilder()
+		.setCOlor(color.green)
+		.setTitle('')
+	  
+	  
+	  if(!server) {
+	      	  this.client.database.Guilds({
+			  _id: guild.name,
+		  }).save().then(msg => {
+			  
+
+	          })
+	  }//if !server
+       }
 }
