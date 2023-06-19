294a7253adce734bbeae5c1ed70ab47cabd3f17b
Henrique Batista <offhenriquebj@gmail.com>
2023-06-18 23:54:21 -0300

    Update sendMessage.js

--- commands/messenger/sendMessage.js
+++ commands/messenger/sendMessage.js
@@ -36,6 +36,7 @@ async function sendCommandArgument() {
   await page.waitForNavigation();
 
   // Localizar a conversa específica
+  await page.click('div')
   const conversationSelector = '#mount_0_0_Xm > div > div > div > div.x9f619.x1n2onr6.x1ja2u2z > div > div > div > div.x78zum5.xdt5ytf.x10cihs4.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.x2lah0s.x1nhvcw1.x1qjc9v5.xozqiw3.x1q0g3np.x78zum5.x1iyjqo2.x1t2pt76.x1n2onr6.x1ja2u2z > div.x9f619.x1n2onr6.x1ja2u2z.xdt5ytf.x193iq5w.xeuugli.x1r8uery.x1iyjqo2.xs83m0k.x78zum5.x1t2pt76 > div > div > div.x1ja2u2z.x9f619.x78zum5.xdt5ytf.x193iq5w.x1l7klhg.x1iyjqo2.xs83m0k.x2lwn1j.xcrg951.x6prxxf.x85a59c.x6ikm8r.x10wlt62.x1n2onr6.x1gvwcb > div > div > div > div.x1uvtmcs.x4k7w5x.x1h91t0o.x1beo9mf.xaigb6o.x12ejxvf.x3igimt.xarpa2k.xedcshv.x1lytzrv.x1t2pt76.x7ja8zs.x1n2onr6.x1qrby5j.x1jfb8zj > div > div > div:nth-child(2) > div > div > div.x78zum5.x1iyjqo2.x6q2ic0 > div.xmjcpbm.x107yiy2.xv8uw2v.x1tfwpuw.x2g32xy.x9f619.x1iyjqo2.xeuugli > div > div > div.xzsf02u.x1a2a7pz.x1n2onr6.x14wi4xw.x1iyjqo2.x1gh3ibb.xisnujt.xeuugli.x1odjw0f.notranslate > p';
   await page.waitForSelector(conversationSelector);
   await page.click(conversationSelector);
@@ -45,6 +46,8 @@ async function sendCommandArgument() {
 
   const messageInputSelector = '[aria-label="Mensagem"]';
   const commandMessage = args[0]
+
+	await page.click('div')
   
   await page.type(messageInputSelector, commandMessage);
   await page.keyboard.press('Enter');
