d1f112a95956ff15a9ba107aa54240116ed0f178
Henrique Batista <offhenriquebj@gmail.com>
2023-06-22 22:57:04 -0400

    Update sendMessage.js

--- commands/messenger/sendMessage.js
+++ commands/messenger/sendMessage.js
@@ -28,7 +28,7 @@ async function sendCommandArgument() {
       await page.setBypassCSP(true);
       await page.setRequestInterception(true);
       page.on('request', request => {
-        if (request.resourceType() === 'image') {
+        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() === 'font') {
           request.abort();
         } else {
           request.continue();
