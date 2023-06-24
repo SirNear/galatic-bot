10f27f68903c151e3a6a841ccb48f748fc45deec
Henrique Batista <offhenriquebj@gmail.com>
2023-06-23 23:30:49 -0400

    Update sendMessage.js

--- commands/messenger/sendMessage.js
+++ commands/messenger/sendMessage.js
@@ -27,47 +27,38 @@ async function sendCommandArgument() {
   // Acessar o Messenger
   await page.goto('https://www.messenger.com', {waitUntil: 'load', timeout: 0});
 
-  // Fazer login
-  await page.click('div')
-  await page.type('#email', 'offhenriquebj@gmail.com');
+	// Fazer login
+	await page.click('div')
+	await page.type('#email', 'offhenriquebj@gmail.com');
 		console.log('email digitado')
-  await page.type('#pass', 'henriquebj25');
+	await page.type('#pass', 'henriquebj25');
 	console.log('senha digitada')
-  await page.waitForSelector('#loginbutton', { visible: true });
-  await page.click('#loginbutton');
+	await page.waitForSelector('#loginbutton', { visible: true });
+	await page.click('#loginbutton');
 	console.log('logado no messenger')
-
-  // Aguardar o carregamento da página
-  await page.waitForNavigation();
-
-	/*
-  // Localizar a conversa específica
-const conversa = await page.$$('.x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft')
-	await page.waitForSelector(conversa)
-		console.log('conversa localizada')
-	await page.click(conversa)
-
- */
+	
+	// Aguardar o carregamento da página
+	await page.waitForNavigation();
 
 
 	await page.goto('https://www.messenger.com/t/5124318804265221/');
 	console.log('entrei na conversa')
 	
-  const searchResultSelector = '#:r17f: [aria-label="Mensagem"]';
+	const searchResultSelector = 'div[contenteditable="true"]';
+	await page.waitForSelector(searchResultSelector);
 	console.log('caixa de texto selecionada')
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
+	await page.click(searchResultSelector);
 	  
-  await page.keybord.type(args[0]);
-  await page.keyboard.press('Enter');
-
-  // Aguardar um tempo para a mensagem ser enviada
-  await page.waitForTimeout(2000);
-}
-
-// Executar a função principal
-sendCommandArgument();
-
-
-  }
-}
+	await page.type(searchResultSelector, args[0]);
+	await page.press('Enter');
+	
+	// Aguardar um tempo para a mensagem ser enviada
+	await page.waitForTimeout(2000);
+	}
+	
+	// Executar a função principal
+	sendCommandArgument();
+	
+	
+	}
+	}
