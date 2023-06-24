508fa0953975e25c1e8c2aff874e6361150828b8
Henrique Batista <offhenriquebj@gmail.com>
2023-06-23 23:49:54 -0400

    Update sendMessage.js

--- commands/messenger/sendMessage.js
+++ commands/messenger/sendMessage.js
@@ -44,7 +44,7 @@ async function sendCommandArgument() {
 	await page.goto('https://www.messenger.com/t/5124318804265221/');
 	console.log('entrei na conversa')
 	
-	const searchResultSelector = '#mount_0_0_1X';
+	const searchResultSelector = '[aria-label="Mensagem"]';
 	await page.waitForSelector(searchResultSelector);
 	console.log('caixa de texto selecionada')
 	await page.click(searchResultSelector);
