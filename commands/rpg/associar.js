5007297d223a56ed64b44e336f52f22e88421ea4
SirNear <offhenriquebj@gmail.com>
2025-08-23 01:56:51 -0300

    PROGRAMAÇÃO FRONT preparação para sistematizar fichas

--- commands/rpg/associar.js
+++ commands/rpg/associar.js
@@ -12,19 +12,55 @@ module.exports = class associar extends Command {
 			aliases: ['ass', 'ac', 'registrar'],
 			UserPermission: [""],
 			clientPermission: null,
-			OnlyDevs: false,
-			structure: 'membro jogador'
+			OnlyDevs: true,
+			structure: '@membro jogador'
 		})
 	}
   
 async run({ message, args, client, server}) {
 
+	let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+	let msgJogador = args.slice(1).join(' ');
+
 	if (!message.member.roles.cache.has('731974690125643869') && message.guild.id == '731974689798488185' ) {  message.reply({content: `${error.noAdmod}`}) }
+	if(!member || !msgJogador.trim()) return error.helpCmd(server, this.config, message);
 
-	let msgArg = args.slice(0).join(' '); //ARGUMENTO DIGITADO PELO USUARIO
-	!msgArg.trim() ? error.helpCmd(server, this.config, message) : null;
+	//? --------------- EMBED ZONE ---------------------
+
+	let embedConfirma = new EmbedBuilder()
+		.setColor(color.moderation)
+		.setTitle('<a:Nowoted:1408666836538495017> | DESEJA REGISTRAR ESSE USUÁRIO?')
+		.setDescription(`O membro mencionado(${member}) será atribuido ao jogador \`\`${msgJogador}\`\`. Isso signifca que todo seu registro atual de ficha, aparências e futuras modificações ou dados passarão à pertencer e serem associados a ele. **Deseja continuar?** `)
+		.setFooter({text: 'Use os botões para confirmar ou cancelar a operação'})
+
+	let embedRegistrado = new EmbedBuilder()
+		.setColor(color.moderation)
+		.setTitle('<a:CaughtIn4K:1408664555382509598> | USUÁRIO REGISTRADO! ')
+		.setDescription(`Membro ${member} associado ao jogador \`\`${msgJogador}\`\` `)
+
+	const botaoConfirma = new ActionRowBuilder() 
+		.addComponents(
+			new ButtonBuilder().setCustomId('confirma').setLabel('SIM').setStyle(ButtonStyle.Success),
+			new ButtonBuilder().setCustomId('nega').setLabel('CANCELAR').setStyle(ButtonStyle.Danger),
+		);
+
+	//? --------------- EMBED ZONE ---------------------
 
 
+	message.delete()
+	const msgPadrao = await message.reply({embeds: [embedConfirma], components: [botaoConfirma], ephemeral: true})
+
+	const coletorBotao = msgPadrao.createMessageComponentCollector({ filter: i => i.user.id === message.author.id, time: 60000 }); //60s de espera
+
+	coletorBotao.on('collect', async i => {
+		if(i.customId == 'confirma') {
+			msgPadrao.edit({embeds: [embedRegistrado], components: []})
+			coletorBotao.stop('closed');
+		}else { 
+			msgPadrao.edit({content: '<a:cdfpatpat:1407135944456536186> | Tudo bem! Você pode registar mais tarde!', components: [], embeds: []})
+			coletorBotao.stop('closed');
+		}
+	})
 
   }
 }
