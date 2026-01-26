8183106e79a22c549575eb4014aef340a4337706
SirNear <offhenriquebj@gmail.com>
2026-01-26 01:10:11 -0400

    [ - ] remoção guild leave

--- events/ready.js
+++ events/ready.js
@@ -7,11 +7,6 @@ module.exports = class Ready {
 
   async run() {
     console.log(`Bot ${this.client.user.tag} está online!`);
-    const guilds = this.client.guilds.cache;
-    
-    guilds.forEach(guild => { 
-      if(guild.id !== "731974689798488185" && guild.id !== "930871020557062162") return guild.leave() 
-    });
 
     await this.client.registerSlashCommands();
     await this.client.loadQuestCollectors();
